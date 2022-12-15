27661dc1d6d2a6a7738a4ff9a2b978a514de1b39
Quinn Zieltjens <quinnzieltjens@gmail.com>
2022-12-15 11:43:48 +0100

    Add project files.

---
+++ J1P2_PRO_TextAdventure/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace J1P2_PRO_TextAdventure //the "location" of this script
+{
+    internal class Program //defines a class
+    {
+        private readonly Command[] commands = {
+            new("go", new string[] { "north", "east", "south", "west", "back" })
+        };
+
+
+        static void Main(string[] _args) //defines a static method which means that the method is accessible from anywhere the class is
+        {
+            Program program = new();
+
+            while (true)
+            {
+                break;
+            }
+        }
+
+        public bool IsCommand(string _input)
+        {
+            foreach (Command command in commands)
+            {
+                if (command.IsThisCommand(_input) == true)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
