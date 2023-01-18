bbf4499d948faee4cbaa3b2a54a92fac65fb8d2b
Quinn Zieltjens <quinnzieltjens@gmail.com>
2023-01-18 13:44:49 +0100

    Commente-d player class

--- J1P2_PRO_TextAdventure/Assets/Environment/Player.cs
+++ J1P2_PRO_TextAdventure/Assets/Environment/Player.cs
@@ -38,10 +38,10 @@
 
 
         /// <summary>
-        /// 
+        /// used to get the position & items of the player
         /// </summary>
-        /// <param name="x">the x position of the player</param>
-        /// <param name="y">the y position of the player</param>
+        /// <param name="_x">the x position of the player</param>
+        /// <param name="_y">the y position of the player</param>
         public Player(int _x, int _y)
         {
             pos = new int[2] { _x, _y }; //assigns an array with the size of 2 and the parameters x, y to the variable
@@ -58,26 +58,30 @@
         /// <param name="_dY">distance of the new player position in the y direction</param>
         public void Move(int _dX, int _dY, World _world)
         {
-            int[] newPos = new int[2] { _dX + pos[0], _dY + pos[1] };
-            Tile tileTryMovedTo;
+            int[] newPos = new int[2] { _dX + pos[0], _dY + pos[1] }; //gets the new position
             int x;
             int y;
+            Tile tileTryMovedTo;
 
-            tileTryMovedTo = _world.GetTile(newPos[0], newPos[1]);
+            tileTryMovedTo = _world.GetTile(newPos[0], newPos[1]); //gets the tile that is tried to be move to
 
             x = newPos[0];
             y = newPos[1];
 
-            if (tileTryMovedTo.CanEnter(this))
+            if (tileTryMovedTo.CanEnter(this)) //checks if the tile moved to can be entered
             {
-                pos[0] = x;
+                pos[0] = x; //sets x to the new position
                 pos[1] = y;
             }
         }
 
-        public (int x, int y) GetPosition()
+        /// <summary>
+        /// gets the players position
+        /// </summary>
+        /// <returns></returns>
+        public (int x, int y) GetPosition() //declares a method with a tuple as return type
         {
-            return (pos[0], pos[1]);
+            return (pos[0], pos[1]); //defines a tuple and returns it
         }
     }
 }
