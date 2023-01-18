5cc25edf7fdddaa2540f79ec45c64c24e7434b7e
Quinn Zieltjens <quinnzieltjens@gmail.com>
2023-01-18 09:50:42 +0100

    Changed MainLoop

--- J1P2_PRO_TextAdventure/GameScripts/MainLoop.cs
+++ J1P2_PRO_TextAdventure/GameScripts/MainLoop.cs
@@ -28,18 +28,50 @@ namespace J1P2_PRO_TextAdventure.GameScripts
         /// <summary>
         /// start of MainLoop
         /// </summary>
-        protected override void OnStart()
+        public void Start()
         {
             Console.ForegroundColor = ConsoleColor.DarkGray;
             Console.WriteLine("hint: type \"help\" if you're stuck.");
             Console.ResetColor();
+
+            Loop();
+            OnEnd();
+        }
+
+        /// <summary>
+        /// gets player's input & tries to run the corresponding command
+        /// </summary>
+        private void Loop()
+        {
+            while (LoopCondition())
+            {
+                string input;
+                bool commandSuccess = false;
+
+                input = GetInput("What do you want to do?");
+
+                foreach (Command command in commands) //loops through all commands 
+                {
+                    if (command.IsCommand(input))
+                    {
+                        command.Run();
+                        commandSuccess = true;
+                        break; //breaks out of the loop
+                    }
+                }
+
+                if (commandSuccess == false)
+                {
+                    Console.WriteLine($"You don't know how to: \"{input}\".");
+                }
+            }
         }
 
         /// <summary>
         /// checks if the player is on the mountain
         /// </summary>
         /// <returns><see langword="true"/> if the player's position is on a mountain tile. Otherwise <see langword="false"/></returns>
-        protected override bool LoopCondition()
+        private bool LoopCondition()
         {
             Tile playerTile = world.GetPlayerTile();
 
@@ -49,59 +81,6 @@ namespace J1P2_PRO_TextAdventure.GameScripts
             }
 
             return true;
-        }
-
-        /// <summary>
-        /// gets player's input & tries to run the corresponding command
-        /// </summary>
-        protected override void DuringLoop()
-        {
-            string input;
-            bool commandSuccess = false;
-
-            input = GetInput("What do you want to do?");
-
-            foreach (Command command in commands) //loops through all commands 
-            {
-                if (command.IsCommand(input))
-                {
-                    command.Run();
-                    commandSuccess = true;
-                    break; //breaks out of the loop
-                }
-            }
-
-            if (commandSuccess == false)
-            {
-                Console.WriteLine($"You don't know how to: \"{input}\".");
-            }
-        }
-
-        /// <summary>
-        /// prints the final dialogue.
-        /// </summary>
-        protected override void OnEnd()
-        {
-            Dialogue endDialogue;
-            Dialogue finalDialogue;
-
-            endDialogue = new(
-                "it was a excruciating climb, but Jesse managed it just because of the beans.",
-                "Jesse inspected the RV, the equipment didn't survive but luckily the built cool-box protected some food and water from the flames.",
-                "Jesse started walking.",
-                "and walking..",
-                "and walking...",
-                "after a full week of walking Jesse ran out of water",
-                "Then finally, a car drove by.",
-                "Jesse tried to hitch hike and the car stopped",
-                "Jesse explained the situation and the person was glad to help");
-            endDialogue.Start();
-
-
-            finalDialogue = new("Finally when reaching Jesse's family, Jesse was very happy to see them and told them all about his adventure.")
-            { ContinuePrompt = "the end." };
-
-            finalDialogue.Start();
         }
 
         private string GetInput(string _message)
@@ -127,6 +106,33 @@ namespace J1P2_PRO_TextAdventure.GameScripts
             Console.WriteLine(' ');
 
             return input;
+        }
+
+        /// <summary>
+        /// prints the final dialogue.
+        /// </summary>
+        private void OnEnd()
+        {
+            Dialogue endDialogue;
+            Dialogue finalDialogue;
+
+            endDialogue = new(
+                "it was a excruciating climb, but Jesse managed it just because of the beans.",
+                "Jesse inspected the RV, the equipment didn't survive but luckily the built cool-box protected some food and water from the flames.",
+                "Jesse started walking.",
+                "and walking..",
+                "and walking...",
+                "after a full week of walking Jesse ran out of water",
+                "Then finally, a car drove by.",
+                "Jesse tried to hitch hike and the car stopped",
+                "Jesse explained the situation and the person was glad to help");
+            endDialogue.Start();
+
+
+            finalDialogue = new("Finally when reaching Jesse's family, Jesse was very happy to see them and told them all about his adventure.")
+            { ContinuePrompt = "the end." };
+
+            finalDialogue.Start();
         }
     }
 }
